4362d404061b54c807ee528ea8eb225085a72a92
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-26 12:21:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,12 @@ class App < Sinatra::Base
 
   get '/newteam' do
     @teamName = params["teamName"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["center"]
     erb :newteam
   end
 end
